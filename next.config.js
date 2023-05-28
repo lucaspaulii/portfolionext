7c935f4b92856445cafd3a22b6e12d49187ced42
lucaspaulii <lucaspauli22@gmail.com>
2023-05-28 20:50:18 -0300

    fix: next config to deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "standalone",
-  distDir: "build",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
